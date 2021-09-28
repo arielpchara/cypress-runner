bbeb0039505262d885a377cb20b07a3e576016bd
Ariel Pchara <ariel.pchara@southsystem.com.br>
2021-09-28 09:54:59 -0300

    hello test

--- cypress/integration/hello.spec.js
+++ cypress/integration/hello.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
-describe('hello', () => {
-    it('should response hello with name', () => {
+describe('function :: hello :: ', () => {
+    test('should response hello with name', () => {
         cy.request('GET', '/').should((response) => {
             expect(response.body.name).to.equal('Hello Foo')
         })
